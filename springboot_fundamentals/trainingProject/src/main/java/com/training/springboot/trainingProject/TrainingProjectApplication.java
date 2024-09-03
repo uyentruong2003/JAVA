e9de4b04aab4bb1678ecb6fddb259e9b6cd7dc99
uyentruong2003 <uyenkaty.truong@gmail.com>
2024-09-03 15:18:05 -0500

    springboot

---
+++ springboot_fundamentals/trainingProject/src/main/java/com/training/springboot/trainingProject/TrainingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.training.springboot.trainingProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingProjectApplication.class, args);
+	}
+
+}
---
+++ springboot_fundamentals/trainingProject/src/test/java/com/training/springboot/trainingProject/TrainingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.springboot.trainingProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
